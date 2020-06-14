90e80ba68ff4def32e1c8bb101fe8f09a6d85611
Slagee <lukas.lazar.42@seznam.cz>
2020-06-14 22:07:42 +0200

    Slider complete, Rooms tab formatted

--- client/src/app/components/rooms/rooms.component.ts
+++ client/src/app/components/rooms/rooms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlus, faHome, faSquare, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Room } from 'src/app/models/room.model';
+import { Options } from '@m0t0r/ngx-slider';
 import { RestApiService } from 'src/app/services/rest-api.service';
 
 @Component({
@@ -15,6 +16,13 @@ export class RoomsComponent implements OnInit {
   faSquare = faSquare;
   faTimes = faTimes;
 
+  sliderValue: number = 23.0;
+  sliderOptions: Options = {
+    floor: 15,
+    ceil: 30,
+    step: 0.1
+  }
+
   roomsData: Array<Room> = [];
   selectedRoom: Room;
 
@@ -25,7 +33,9 @@ export class RoomsComponent implements OnInit {
   ngOnInit(): void {
     this.restApiService.getAllRoomData()
     .subscribe(
-      (roomsData: Array<Room>) => this.roomsData = roomsData,
+      (roomsData: Array<Room>) => {
+        this.roomsData = roomsData;
+        this.selectedRoom = roomsData[0]},
       (error) => console.log(error),
       () => {}
     );
@@ -33,7 +43,6 @@ export class RoomsComponent implements OnInit {
 
   onSelect(room: Room): void {
     this.selectedRoom = room;
-    console.log(room);
+    console.log(room.name);
   }
-
 }
--- client/src/app/models/room.model.ts
+++ client/src/app/models/room.model.ts
@@ -2,5 +2,6 @@ export class Room {
     name: string | null = null;
     currentTemperature: number | null = null;
     selectedTemperature: number | null = null;
+    powerConsumption: number | null = null;
     radiatorState: boolean | false = false;
 }
