2d64639369513f751ff47bebf81dc99a1bf16ba7
Slagee <lukas.lazar.42@seznam.cz>
2020-06-30 18:54:17 +0200

    Cors for stats

--- src/main/java/com/swinz/swinz/api/StatisticsController.java
+++ src/main/java/com/swinz/swinz/api/StatisticsController.java
@@ -2,10 +2,12 @@ package com.swinz.swinz.api;
 
 import com.swinz.swinz.service.StatisticsPseudoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class StatisticsController {
 
